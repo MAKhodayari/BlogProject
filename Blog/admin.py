79c422ad77b93b65685cdb021f793ebbe260f78d
Mohammad Ali Khodayari <MAKhodayari99@Gmail.com>
2023-10-04 23:48:25 +0330

    Update Admin

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,90 +1,110 @@
 from django.contrib import admin
-from django.template.defaultfilters import truncatechars
+from django.db.models import Count
+from django.template.defaultfilters import truncatewords
 
 from Blog.actions import *
 from Blog.models import *
 
+admin.site.disable_action('delete_selected')
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-	fields = ['title', 'content', 'category', 'tag', 'author', 'previous_post']
-	list_display = [
-		'title', 'shorten_content', 'get_categories', 'get_tags', 'author', 'previous_post', 'likes', 'dislikes',
-		'get_comment_count', 'views', 'is_deleted', 'is_published'
-	]
-	list_filter = ['category', 'is_deleted', 'is_published']
-	search_fields = ['title', 'content', 'author__username']
-	ordering = ['-created_at']
+
+class BaseAdmin(admin.ModelAdmin):
+	list_display = ['is_deleted', 'is_published']
+	list_filter = ['is_deleted', 'is_published']
 	actions = [delete, include, publish]
 	list_per_page = 10
 
+
+class CategoryTagAdmin(BaseAdmin):
+	fields = ['title']
+	list_display = ['title', 'get_post_count'] + BaseAdmin.list_display
+	ordering = ['title']
+
+	def get_queryset(self, request):
+		queryset = super().get_queryset(request)
+		queryset = queryset.annotate(post_count=Count('post'))
+		return queryset
+
+	def get_post_count(self, obj):
+		return obj.post_count
+
+	get_post_count.short_description = 'post count'
+
+
+@admin.register(Post)
+class PostAdmin(BaseAdmin):
+	fields = ['title', 'content', 'category', 'tag', 'author', 'previous_post']
+	list_display = [
+		'shorten_title', 'shorten_content', 'get_category', 'get_tag', 'author', 'shorten_post', 'likes', 'dislikes',
+		'get_comment_count', 'views'
+	] + BaseAdmin.list_display
+	list_filter = ['category', 'tag'] + BaseAdmin.list_filter
+	search_fields = ['title', 'content', 'author__username']
+	ordering = ['-created_at']
+
+	def get_queryset(self, request):
+		queryset = super().get_queryset(request)
+		queryset = queryset.annotate(comment_count=Count('comment'))
+		return queryset
+
+	def shorten_title(self, obj):
+		return truncatewords(obj.title, 10)
+
 	def shorten_content(self, obj):
-		return truncatechars(obj.content, 26)
+		return truncatewords(obj.content, 15)
 
-	shorten_content.short_description = 'content'
+	def shorten_post(self, obj):
+		if obj.previous_post is not None:
+			return truncatewords(obj.previous_post.title, 10)
+		return
 
-	def get_categories(self, obj):
+	def get_category(self, obj):
 		return ', '.join([c.title for c in obj.category.all()])
 
-	get_categories.short_description = 'categories'
-
-	def get_tags(self, obj):
+	def get_tag(self, obj):
 		return ', '.join([t.title for t in obj.tag.all()])
 
-	get_tags.short_description = 'tags'
-
 	def get_comment_count(self, obj):
-		return obj.comment_set.count()
+		return obj.comment_count
 
+	shorten_title.short_description = 'title'
+	shorten_content.short_description = 'content'
+	shorten_post.short_description = 'previous_post'
+	get_category.short_description = 'category'
+	get_tag.short_description = 'tag'
 	get_comment_count.short_description = 'comment count'
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-	fields = ['title']
-	list_display = ['title', 'get_post_count', 'is_deleted', 'is_published']
-	ordering = ['title']
-	actions = [delete, include, publish]
-
-	def get_post_count(self, obj):
-		return obj.post_set.count()
-
-	get_post_count.short_description = 'post count'
-
-
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-	fields = ['title']
-	list_display = ['title', 'get_post_count', 'is_deleted', 'is_published']
-	ordering = ['title']
-	actions = [delete, include, publish]
-
-	def get_post_count(self, obj):
-		return obj.post_set.count()
-
-	get_post_count.short_description = 'post count'
-
-
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(BaseAdmin):
 	fields = ['author', 'content', 'post', 'previous_comment']
 	list_display = [
-		'author', 'shorten_content', 'post', 'shorten_comment', 'likes', 'dislikes', 'is_deleted', 'is_published'
-	]
+		'author', 'shorten_content', 'shorten_post', 'shorten_comment', 'likes', 'dislikes'
+	] + BaseAdmin.list_display
 	search_fields = ['author', 'content', 'post__title']
 	ordering = ['-post__title', '-created_at']
-	actions = [delete, include, publish]
-	list_per_page = 10
 
 	def shorten_content(self, obj):
-		return truncatechars(obj.content, 26)
+		return truncatewords(obj.content, 15)
 
-	shorten_content.short_description = 'content'
+	def shorten_post(self, obj):
+		return truncatewords(obj.post.title, 10)
 
 	def shorten_comment(self, obj):
 		if obj.previous_comment is not None:
-			return truncatechars(obj.previous_comment, 26)
-		else:
-			return
+			return truncatewords(obj.previous_comment, 10)
+		return
 
+	shorten_content.short_description = 'content'
+	shorten_post.short_description = 'post'
 	shorten_comment.short_description = 'previous comment'
+
+
+@admin.register(Category)
+class CategoryAdmin(CategoryTagAdmin):
+	pass
+
+
+@admin.register(Tag)
+class TagAdmin(CategoryTagAdmin):
+	pass
